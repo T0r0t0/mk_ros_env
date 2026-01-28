8be2feec9241d4f4e602b31ec7517c738f83a760
T0r0t0 <toscan.c.p@gmail.com>
2026-01-28 21:22:14 +0100

    Add docker installation part, if not already install

--- install.sh
+++ install.sh
@@ -12,5 +12,39 @@ else
     echo "$(pwd) is already in PATH in .bashrc"
 fi
 
+
+echo "Check if docker installed ... "
+
+if !command -v docker &> /dev/null; then
+    echo "Docker not found"
+    echo "Installing docker ..."
+    # Add Docker's official GPG key:
+    sudo apt update
+    sudo apt install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+    sudo apt update
+
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+else
+    echo Docker found.
+fi
+
+echo installation finished
+
+
 # Reload .bashrc
 exec bash
+
